1c34b63e4e4b2d469c434057af011ca9aba3d374
Topper Bowers <topper@toppingdesign.com>
2022-05-30 11:17:13 +0200

    initial

--- contracts/contracts/BadgeOfAssembly.sol
+++ contracts/contracts/BadgeOfAssembly.sol
@@ -2,17 +2,35 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-
-contract BadgeOfAssembly is ERC1155URIStorage {
-
-    
+contract BadgeOfAssembly is ERC1155URIStorage, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC1155("https://badge-of-assembly.netlify.app/api/boa/{id}.json") {
-
+        _setupRole(MINTER_ROLE, msg.sender);
     }
 
-    function contractURI() public view returns (string memory) {
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControl, ERC1155)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function contractURI() public pure returns (string memory) {
         return "https://badge-of-assembly.netlify.app/api/boa";
+    }
+    
+    function mint(address to, uint256 amount) public {
+        // Check that the calling account has the minter role
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, 0, amount, "");
     }
 }
