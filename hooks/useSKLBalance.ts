71ab2a4e1ce1b4d6acb9278357c49d65d82ba651
Topper Bowers <topper@toppingdesign.com>
2022-06-01 22:24:54 +0200

    formatting

--- hooks/useSKLBalance.ts
+++ hooks/useSKLBalance.ts
@@ -1,7 +1,7 @@
 import { ethers, BigNumber } from "ethers";
 import { chain } from "wagmi";
-import { useQuery } from "react-query"
-import { getMainnetSdk } from '@dethcrypto/eth-sdk-client' // yay, our SDK! It's tailored especially for our needs
+import { useQuery } from "react-query";
+import { getMainnetSdk } from "@dethcrypto/eth-sdk-client"; // yay, our SDK! It's tailored especially for our needs
 
 const SKL_ADDRESS = "0x00c83aeCC790e8a4453e5dD3B0B4b3680501a7A7";
 
@@ -10,24 +10,27 @@ const mainnetProvider = new ethers.providers.AlchemyProvider(
   process.env.NEXT_PUBLIC_ALCHEMY_KEY
 );
 
-const sdk = getMainnetSdk(mainnetProvider)
+const sdk = getMainnetSdk(mainnetProvider);
 
 const useSKLBalance = (address?: string) => {
   const fetchBalance = async () => {
     const [balance, staked] = await Promise.all([
-      mainnetProvider.send('alchemy_getTokenBalances', [address, [SKL_ADDRESS]]),
-      sdk.skl.callStatic.getAndUpdateDelegatedAmount(address!)
-    ])
-    const liquid = BigNumber.from(balance.tokenBalances[0].tokenBalance)
+      mainnetProvider.send("alchemy_getTokenBalances", [
+        address,
+        [SKL_ADDRESS],
+      ]),
+      sdk.skl.callStatic.getAndUpdateDelegatedAmount(address!),
+    ]);
+    const liquid = BigNumber.from(balance.tokenBalances[0].tokenBalance);
     return {
       staked,
       liquid,
       total: staked.add(liquid),
-    }
-  }
-  return useQuery(['skl-balance', address], fetchBalance, {
-    enabled: !!address
-  })
+    };
+  };
+  return useQuery(["skl-balance", address], fetchBalance, {
+    enabled: !!address,
+  });
 };
 
 export default useSKLBalance;
